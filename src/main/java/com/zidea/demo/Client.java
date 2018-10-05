316ec90c721188b90127c923293dd4603a529578
jangwoo <zidea2015@163.com>
2018-10-05 14:28:21 +0800

    execute class file

--- src/main/java/com/zidea/demo/Client.java
+++ src/main/java/com/zidea/demo/Client.java
@@ -1,8 +1,24 @@
 package com.zidea.demo;
 
-public class Client{
+import com.zidea.demo.model.Tut;
 
-    public static void main(String[] args){
-        System.out.print("hello world");
+public class Client {
+
+    private Tut tut;
+
+    public Client() {
+    };
+
+    /**
+     * @return the tut
+     */
+    public Tut getTut() {
+        return tut;
+    }
+
+    public static void main(String[] args) {
+        Tut tut = new Tut();
+        tut.setName("javascript");
+        System.out.print("hello world" + tut.getName() + " tut");
     }
 }
---
+++ src/main/java/com/zidea/demo/model/Tut.java
@@ -0,0 +1,25 @@
+package com.zidea.demo.model;
+
+public class Tut {
+
+    private String name;
+
+    public Tut() {
+
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}

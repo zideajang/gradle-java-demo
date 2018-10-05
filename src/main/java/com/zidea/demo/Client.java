7e4e7e73988aae78c9ae06694467cc3eb8ad4e69
jangwoo <zidea2015@163.com>
2018-10-05 14:36:58 +0800

    funtional programming

--- src/main/java/com/zidea/demo/Client.java
+++ src/main/java/com/zidea/demo/Client.java
@@ -1,5 +1,9 @@
 package com.zidea.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
 import com.zidea.demo.model.Tut;
 
 public class Client {
@@ -17,6 +21,17 @@ public class Client {
     }
 
     public static void main(String[] args) {
+
+        List<String> tuts = new ArrayList<>();
+
+        tuts.add("javascript");
+        tuts.add("cpp");
+        tuts.add("java");
+
+        tuts.stream().forEach(str -> {
+            System.out.println(str);
+        });
+
         Tut tut = new Tut();
         tut.setName("javascript");
         System.out.print("hello world" + tut.getName() + " tut");

414af5aa80226b29c3934c160421d1b6e006667a
jangwoo <zidea2015@163.com>
2018-10-05 15:54:47 +0800

    fuctional programming validation

--- src/main/java/com/zidea/demo/Client.java
+++ src/main/java/com/zidea/demo/Client.java
@@ -3,37 +3,32 @@ package com.zidea.demo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 
 import com.zidea.demo.model.Tut;
 
 public class Client {
 
-    private Tut tut;
+    final Pattern emailPattern = Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
 
-    public Client() {
-    };
+    final Function<String, Boolean> emailChecker = s -> emailPattern.matcher(s).matches();
 
-    /**
-     * @return the tut
-     */
-    public Tut getTut() {
-        return tut;
+    public void testMail(String email) {
+        // if (emailPattern.matcher(email).matches()) {
+        // System.out.println("validated email");
+        // } else {
+        // System.out.println("unvalidated email");
+        // }
+        if (emailChecker.apply(email)) {
+            System.out.println("validated email...");
+        } else {
+            System.out.println("unvalidated email");
+        }
     }
 
     public static void main(String[] args) {
-
-        List<String> tuts = new ArrayList<>();
-
-        tuts.add("javascript");
-        tuts.add("cpp");
-        tuts.add("java");
-
-        tuts.stream().forEach(str -> {
-            System.out.println(str);
-        });
-
-        Tut tut = new Tut();
-        tut.setName("javascript");
-        System.out.print("hello world" + tut.getName() + " tut");
+        Client client = new Client();
+        client.testMail("zidea2015@163.com");
     }
 }
